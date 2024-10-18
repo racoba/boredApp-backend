4fa5b79218942c526ddd72351bf4216050636c32
Bruno Racobaldo Santos <60931988+racoba@users.noreply.github.com>
2024-10-17 21:41:45 -0300

    Fixed router

--- src/app/controllers/index.ts
+++ src/app/controllers/index.ts
@@ -1,3 +1,4 @@
 export { default as userRouter } from "./UserController";
 export { default as authRouter } from "./AuthController";
 export { default as taskRouter } from "./TaskController";
+export { default as userTaskRouter } from "./UserTaskController";
--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { authRouter, userRouter, taskRouter } from "../controllers/index";
+import { authRouter, userRouter, taskRouter, userTaskRouter , } from "../controllers/index";
 
 const routers = Router();
 
 routers.use("/users", userRouter);
 routers.use("/tasks", taskRouter);
+routers.use("/userTasks", userTaskRouter);
 routers.use("/auth", authRouter);
 
 export default routers;
